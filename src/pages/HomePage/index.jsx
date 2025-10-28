a63697a5b29d6b4ee3a74f9ba199b03480bb7c3e
rahulsinghchauhanrj <rahulsinghchauhanrj@gmail.com>
2025-10-28 12:50:14 +0000

    Better Video Platform

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,16 +1,28 @@
 import './style.css'
+import { useSelector } from 'react-redux';
 import ListComponent from '../../components/ListComponent';
 import VideoPlayer from '../../components/VideoPlayer';
 
 function HomePage() {
+  const { selectedVideo } = useSelector(state => state.videos);
+
   return (
     <div className="app">
+       <div className="header">
+        <h1>Dev-tube</h1>
+      </div>
       <div className="container">
         <div className="list-wrapper">
           <ListComponent />
         </div>
         <div className="player-wrapper">
           <VideoPlayer />
+            {selectedVideo && (
+              <div className="video-details">
+                <h2>{selectedVideo.title}</h2>
+                <p>{selectedVideo.description}</p>
+              </div>
+            )}
         </div>
       </div>
     </div>
