ee5eba3db46aeb996077730b4df6a9864ae6c700
Rahul Singh Chauhan <rahulsinghchauhanrj@gmail.com>
2025-10-28 16:46:05 +0530

    Alignment between components

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,7 +5,6 @@ import VideoPlayer from '../../components/VideoPlayer';
 function HomePage() {
   return (
     <div className="app">
-      <h1 className="title">Video Player</h1>
       <div className="container">
         <div className="list-wrapper">
           <ListComponent />
